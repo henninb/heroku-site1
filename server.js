be4505d565075c512bc76c1b6018d6ae4a48529e
Brian Henning <henninb@msn.com>
2023-01-02 17:59:26 -0600

    wip

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const fetch = require('node-fetch')
+// import fetch from 'node-fetch';
 //const cors = require('cors');
 //const bodyParser = require('body-parser');
 
@@ -11,6 +13,25 @@ app.use((request, response, next) => {
   next();
 });
 
+app.get('/api/hockey', async (req, res, next) => {
+    // API code will be here
+  const API_ENDPOINT = 'https://fixturedownload.com/feed/json/nhl-2022/minnesota-wild'
+  //const API_ENDPOINT = 'https://catfact.ninja/fact'
+  console.log(API_ENDPOINT)
+  let response
+  try {
+    response = await fetch(API_ENDPOINT)
+    data = await response.json()
+    // handle response
+  } catch (err) {
+    console.log('failure caught');
+    console.log(err);
+    res.send(req,res,err,500);
+  }
+  res.send(data)
+  //res.send("test")
+})
+
 app.listen(port, () => { console.log(`listening on port ${port}`) });
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -19,3 +40,4 @@ app.use(express.static('public'));
 app.post('/api/login', (_req, res) => {
    res.send('api login POST called.');
 });
+
